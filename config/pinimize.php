53365f8b318cbf3aa64b1585aed217552627dc79
Einar Hansen <49709354+einar-hansen@users.noreply.github.com>
2024-08-25 22:37:53 +0200

    MInor fixes

--- config/pinimize.php
+++ config/pinimize.php
@@ -15,6 +15,10 @@ return [
     |   0: no compression,
     |   1: fastest,
     |   9: best
+    |
+    | Disk:
+    |   The disk where the files are located and will be stored. If the disk is set to `null`, then the local filesystem
+    |   will be used.
     */
     'compression' => [
         'default' => env('COMPRESSION_DRIVER', 'gzip'),
@@ -22,10 +26,12 @@ return [
             'gzip' => [
                 'level' => env('GZIP_LEVEL', -1),
                 'encoding' => FORCE_GZIP,
+                'disk' => env('COMPRESSION_DISK', null),
             ],
             'zlib' => [
                 'level' => env('ZLIB_LEVEL', -1),
                 'encoding' => ZLIB_ENCODING_DEFLATE,
+                'disk' => env('COMPRESSION_DISK', null),
             ],
         ],
     ],
--- src/Facades/Decompression.php
+++ src/Facades/Decompression.php
@@ -4,13 +4,16 @@ namespace Pinimize\Facades;
 
 use Closure;
 use Illuminate\Support\Facades\Facade;
+use Pinimize\Contracts\DecompressionContract;
 use Pinimize\Managers\DecompressionManager;
 
 /**
  * @method static DecompressionManager driver($driver = null)
  * @method static DecompressionManager extend($driver, Closure $callback)
  *
- * @see DeompressionManager
+ * @mixin DecompressionContract
+ *
+ * @see DecompressionManager
  */
 class Decompression extends Facade
 {
