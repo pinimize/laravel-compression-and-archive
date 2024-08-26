3e219c121cc09860d5c9720a2e13bb34538f3185
Einar Hansen <49709354+einar-hansen@users.noreply.github.com>
2024-08-26 07:36:28 +0200

    Update readme (#2)
    * Fix type in name
    
    * MInor fixes
    
    * Remove compression readme
    
    * Documentation
    
    * Update README with examples
    
    * Fix table of contents
    
    * Update names of Github Workflows

--- src/Facades/Compression.php
+++ src/Facades/Compression.php
@@ -4,11 +4,14 @@ namespace Pinimize\Facades;
 
 use Closure;
 use Illuminate\Support\Facades\Facade;
+use Pinimize\Contracts\CompressionContract;
 use Pinimize\Managers\CompressionManager;
 
 /**
  * @method static CompressionManager driver($driver = null)
  * @method static CompressionManager extend($driver, Closure $callback)
+ *
+ * @mixin CompressionContract
  *
  * @see CompressionManager
  */
